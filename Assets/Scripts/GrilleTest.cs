848937f924d8d6e60b31299f21e6ed40dbe602e4
lucasmarrel <marrel.lucas@gmail.com>
2017-12-02 13:19:08 -0500

    Correction tuiles

--- Assets/Scripts/GrilleTest.cs
+++ Assets/Scripts/GrilleTest.cs
@@ -12,6 +12,7 @@ public class GrilleTest : MonoBehaviour
 
         List<GameObject> items = new List<GameObject>(GameObject.FindGameObjectsWithTag("solDepart"));
         items.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("sol")));
+        items.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("obstacle")));
         print("Nombre grille : " +items.Count);
     }
 
