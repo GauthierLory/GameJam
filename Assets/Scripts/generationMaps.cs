642b6681588ae6c9b7a31050b2e1f06759fe9d4e
Flo <flolevy33@gmail.com>
2017-12-02 01:18:24 -0500

    Ajout script generation caisse aleatoire (TEST)

--- Assets/Scripts/generationMaps.cs
+++ Assets/Scripts/generationMaps.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class generationMaps : MonoBehaviour {
+public class generationMaps : MonoBehaviour
+{
+    public GameObject caisse;
+    public GameObject[] sols;
+    System.Random rnd;
+    int suivant;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rnd = new System.Random ();
+        for (int i = 0; i < sols.Length; i++)
+        {
+            suivant = rnd.Next(2);
+            print(suivant);
+            if (suivant == 1)
+            {
+                Instantiate(caisse, new Vector2(sols[i].transform.position.x, sols[i].transform.position.y), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
 }
