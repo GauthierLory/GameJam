d39e113d5e947f43e83734610888f07c3836c765
GauthierLory <gauthier.axel.lory@gmail.com>
2017-12-02 02:11:25 -0500

    Menu operationnel

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,26 +7,62 @@ public class Menu : MonoBehaviour {
 
     public Canvas CanvasMenu;
     public Canvas CanvasOption;
+    public Canvas CanvasNiveau;
 
     void Awake()
     {
         CanvasOption.enabled = false;
+        CanvasNiveau.enabled = false;
     }
 
     public void OptionsOn()
     {
         CanvasOption.enabled = true;
+        CanvasNiveau.enabled = false;
         CanvasMenu.enabled = false;
     }
 
     public void ReturnOn()
     {
         CanvasOption.enabled = false;
+        CanvasNiveau.enabled = false;
         CanvasMenu.enabled = true;
+    }
+
+    public void SelectOn()
+    {
+        CanvasOption.enabled = false;
+        CanvasMenu.enabled = false;
+        CanvasNiveau.enabled = true;
     }
 
     public void LoadOn()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Selectlvl1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Selectlvl2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Selectlvl3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Selectlvl4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Selectlvl5()
+    {
+        SceneManager.LoadScene(5);
     }
 }
