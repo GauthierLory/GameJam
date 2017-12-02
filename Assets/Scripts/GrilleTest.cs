94e6882be407e3e59810c954018d224a2d1a5509
lucasmarrel <marrel.lucas@gmail.com>
2017-12-02 13:25:40 -0500

    Suppression obstacle

--- Assets/Scripts/GrilleTest.cs
+++ Assets/Scripts/GrilleTest.cs
@@ -10,9 +10,9 @@ public class GrilleTest : MonoBehaviour
     void Start()
     {
 
-        List<GameObject> items = new List<GameObject>(GameObject.FindGameObjectsWithTag("solDepart"));
-        items.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("sol")));
-        items.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("obstacle")));
+        List<GameObject> items = new List<GameObject>(GameObject.FindGameObjectsWithTag("obstacle"));
+        //items.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("sol")));
+        //items.AddRange(new List<GameObject>(GameObject.FindGameObjectsWithTag("obstacle")));
         print("Nombre grille : " +items.Count);
     }
 
