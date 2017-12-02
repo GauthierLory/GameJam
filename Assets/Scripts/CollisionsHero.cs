eab64b3ede0790659f0addfa1fec6925034998ec
Stocky93420 <navpreet.mail@gmail.com>
2017-12-02 12:21:10 -0500

    Modif

--- Assets/Scripts/CollisionsHero.cs
+++ Assets/Scripts/CollisionsHero.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionsHero : MonoBehaviour {
+    //definition des points à gagner
+    private static int POINTS_POTION = 20;
+    private static int POINTS_ENNEMIS = 60;
+    private static int POINTS_COEUR = 1;
+    private static int POINTS_CRANE_MORT = 10;
+    private static int SCORE_INITIAL = 0;
+    private static int SCORE_GAGNANT = 180;
     
 
 	// Use this for initialization
