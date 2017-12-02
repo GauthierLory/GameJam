1295b3d56b34552fb7be7551c48268880b1f980f
lucasmarrel <marrel.lucas@gmail.com>
2017-12-02 12:25:51 -0500

    Modification du script CollisionsHero

--- Assets/Scripts/CollisionsHero.cs
+++ Assets/Scripts/CollisionsHero.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionsHero : MonoBehaviour {
-    //definition des points à gagner
+
+    //definition des points à gagner ou à perdre
     private static int POINTS_POTION = 20;
     private static int POINTS_ENNEMIS = 60;
-    private static int POINTS_COEUR = 1;
+    private static int POINTS_VITESSE_AUGMENTEE = 20;
+    private static int POINTS_COEUR = 20;
+    private static int POINTS_STOP_HERO = 10;
     private static int POINTS_CRANE_MORT = 10;
+    //definition scores
     private static int SCORE_INITIAL = 0;
+    private static int SCORE_ACTUEL;
     private static int SCORE_GAGNANT = 180;
-    
+    //definition vie
+    private static int POINT_VIE = 1;
+    private static int VIE_INITIALE = 3;
+    private static int VIE_ACTUELLE;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        SCORE_ACTUEL = SCORE_INITIAL;
+        VIE_ACTUELLE = VIE_INITIALE;
 	}
 	
 	// Update is called once per frame
@@ -22,15 +31,29 @@ public class CollisionsHero : MonoBehaviour {
 		
 	}
 
+    //ajout ou supression des points, des vies lors de la collision du hero avec les items
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.tag == "potion")
         {
-
+            SCORE_ACTUEL += POINTS_POTION;
         }
-        else if (collider.tag == "ennemi")
+        else if (collider.tag == "cranemort")
         {
-
+            SCORE_ACTUEL -= POINTS_CRANE_MORT;
+        }
+        else if (collider.tag == "vitesseaugmentee")
+        {
+            SCORE_ACTUEL += POINTS_VITESSE_AUGMENTEE;
+        }
+        else if (collider.tag == "coeur")
+        {
+            SCORE_ACTUEL += POINTS_COEUR;
+            VIE_ACTUELLE += POINT_VIE;
+        }
+        else if (collider.tag == "stophero")
+        {
+            SCORE_ACTUEL += POINTS_STOP_HERO;
         }
     }
 }
