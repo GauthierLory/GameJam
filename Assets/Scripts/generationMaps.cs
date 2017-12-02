334d0ca62504cdc08a43d33c2edb3605fed9fa90
Flo <flolevy33@gmail.com>
2017-12-02 01:54:34 -0500

    Mise en place du script de generation des caisses

--- Assets/Scripts/generationMaps.cs
+++ Assets/Scripts/generationMaps.cs
@@ -2,24 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Ce script genere les caisses sur les maps aleatoirement
 public class generationMaps : MonoBehaviour
 {
+    //Recuperation de l'image de la caisse
     public GameObject caisse;
-    public GameObject[] sols;
+
+    //Creation d'un tableau sol
+    GameObject[] sol;
+
+    //Instantation de la fonction Random
     System.Random rnd;
     int suivant;
 
     // Use this for initialization
     void Start()
     {
+        //Recuperation de tous les gameobjects avec le tag sol
+        sol = GameObject.FindGameObjectsWithTag("sol");
         rnd = new System.Random ();
-        for (int i = 0; i < sols.Length; i++)
+        //de 0 au nombre de sols
+        for (int i = 0; i < sol.Length; i++)
         {
             suivant = rnd.Next(2);
             print(suivant);
             if (suivant == 1)
             {
-                Instantiate(caisse, new Vector2(sols[i].transform.position.x, sols[i].transform.position.y), Quaternion.identity);
+                //Creation des sols
+                Instantiate(caisse, new Vector2(sol[i].transform.position.x, sol[i].transform.position.y), Quaternion.identity);
             }
         }
     }
