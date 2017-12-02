bf54796f8a44d652f39c7dde6571ee010812c085
Stocky93420 <navpreet.mail@gmail.com>
2017-12-01 21:17:01 -0500

    Ajout de sprite / scripts

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [AddComponentMenu("Custom/Editor")]
+
 
 public class Grid : MonoBehaviour
 {
@@ -10,34 +12,33 @@ public class Grid : MonoBehaviour
     public float width = 1.0f;
     public float height = 1.0f;
 
-    public Color color = Color.yellow;
+    public Color color = Color.red;
 
     void OnDrawGizmos()
-    { // va nous permettre de dessiner sur la scene en mode edition
-        Vector3 pos = Camera.current.transform.position; // va chercher la position de la camera
+    {
+        Vector3 pos = Camera.current.transform.position;
 
-        Gizmos.color = color; // indique a Gizmos de quelle couleur on va dessiner
+        Gizmos.color = color;
 
-        //va tracer 500 lignes en ce centrant sur la position de la camera
         for (float y = pos.y - 250.0f; y < pos.y + 250.0f; y += height)
         {
-
             Gizmos.DrawLine
             (
                 new Vector3(-250.0f, Mathf.Floor(y / height) * height, 0.0f),
                 new Vector3(250.0f, Mathf.Floor(y / height) * height, 0.0f)
+
             );
         }
 
-
-        for (float x = pos.x - 250.0f; x < pos.x + 250.0f; x += width)
+        for (float x = pos.x - 250.0f; x < pos.x + 250.0f; x += height)
         {
-
             Gizmos.DrawLine
             (
-                new Vector3(Mathf.Floor(x / width) * width, -250.0f + 0.0f),
-                new Vector3(Mathf.Floor(x / width) * width, 250.0f + 0.0f)
+                new Vector3(Mathf.Floor(x / width) * width, -250.0f, 0.0f),
+                new Vector3(Mathf.Floor(x / width) * width, 250.0f, 0.0f)
+
             );
         }
     }
+
 }
