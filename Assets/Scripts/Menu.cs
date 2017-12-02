8b7d9f535d96a006c86fbbacf6231818b0172712
GauthierLory <gauthier.axel.lory@gmail.com>
2017-12-02 00:48:06 -0500

    Changement de Scene Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas CanvasMenu;
+    public Canvas CanvasOption;
+
+    void Awake()
+    {
+        CanvasOption.enabled = false;
+    }
+
+    public void OptionsOn()
+    {
+        CanvasOption.enabled = true;
+        CanvasMenu.enabled = false;
+    }
+
+    public void ReturnOn()
+    {
+        CanvasOption.enabled = false;
+        CanvasMenu.enabled = true;
+    }
+
+    public void LoadOn()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
