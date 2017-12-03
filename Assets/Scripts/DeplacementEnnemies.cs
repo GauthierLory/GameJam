b5bd25fd6f58bb18b35cf4f42899187ec2ca06a6
lucasmarrel <marrel.lucas@gmail.com>
2017-12-03 11:57:28 -0500

    Ajout du script DeplacementEnnemies 2

--- Assets/Scripts/DeplacementEnnemies.cs
+++ Assets/Scripts/DeplacementEnnemies.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeplacementEnnemies : MonoBehaviour {
+public class DeplacementEnnemies : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Rigidbody2D ennemie;
+
+    // Use this for initialization
+    void Start()
+    {
+        ennemie = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ennemie.transform.Translate(Vector3.left * 0 * Time.deltaTime, Space.World);
+    }
 }
