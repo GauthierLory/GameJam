9b7c4e7b789a05b5b1578a0a8b7e343bd89dbc12
lucasmarrel <marrel.lucas@gmail.com>
2017-12-03 11:50:20 -0500

    Modification du script CollisionsBombe

--- Assets/Scripts/CollisionsBombe.cs
+++ Assets/Scripts/CollisionsBombe.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionsBombe : MonoBehaviour {
+public class CollisionsBombe : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        print("TRIGGER");
+        if (collider.gameObject.tag == "caisse")
+        {
+            Destroy(collider.gameObject);
+        }
+    }
 }
