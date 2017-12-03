7802a7d302796ae95a38339f6f3a5bb86e604cfe
lucasmarrel <marrel.lucas@gmail.com>
2017-12-03 10:28:57 -0500

    Destruction de la bombe au bout de 2.8 secondes

--- Assets/Scripts/ExplosionBombe.cs
+++ Assets/Scripts/ExplosionBombe.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class ExplosionBombe : MonoBehaviour {
 
+    //Initialisation de la vitesse de destruction bombe 
+    private static float VITESSE_BOMBE = 2.8f;
+    private GameObject bombe;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Destruction de la bombe au bout de 3s
+        Destroy(this.gameObject, VITESSE_BOMBE);
+    }
 }
