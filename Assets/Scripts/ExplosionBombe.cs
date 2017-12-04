44100c35cfe4f3b4ad7ffb6d77b3b9512ee630ab
lucasmarrel <marrel.lucas@gmail.com>
2017-12-03 21:15:48 -0500

    FINAL 2

--- Assets/Scripts/ExplosionBombe.cs
+++ Assets/Scripts/ExplosionBombe.cs
@@ -22,7 +22,7 @@ public class ExplosionBombe : MonoBehaviour {
         if (tempExplosion >= 2.7f)
         {
             explosionFeu = Instantiate(explosion, new Vector3(bombe.transform.position.x, bombe.transform.position.y), Quaternion.identity);
-            //Destroy(explosionFeu,0.1f);
+            Destroy(explosionFeu,0.1f);
         }
         //Destruction de la bombe au bout de 2.8s
         Destroy(this.gameObject, VITESSE_BOMBE);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GameJam
+
+Pour voir les règles du jeu lancer le jeu avec la scène Menu.
